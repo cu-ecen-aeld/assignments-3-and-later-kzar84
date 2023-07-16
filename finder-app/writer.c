27de06ff47815d1bc890740ac9b1a243d470983a
223044734 <Cameron.Causer@ge.com>
2023-07-16 18:48:16 -0400

    Adding call to closelog for writer.c

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -7,13 +7,17 @@
 #include <string.h>
 #include <sys/stat.h>
 
+void m_exit(int exit_code) {
+    closelog();
+    exit(exit_code);
+}
 
 void write_file(const char* fln, const char* str) {
     // Open file
     int fd = open(fln, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
     if (fd == -1) {
         syslog(LOG_ERR, "Unable to create file %s", fln);
-        exit(1);
+        m_exit(1);
     }
 
     // Write to file
@@ -21,7 +25,7 @@ void write_file(const char* fln, const char* str) {
     nr = write(fd, str, strlen(str));
     if (nr == -1) {
         syslog(LOG_ERR, "Unable to write %s to %s", str, fln);
-        exit(1);
+        m_exit(1);
     }
     syslog(LOG_DEBUG, "Writing %s to %s", str, fln);
     
@@ -39,12 +43,14 @@ int main(int argc, char *argv[]) {
     // Parse args
     if (argc != 3) {
         syslog(LOG_ERR, "Missing arguments, must provide file path and write string");
-        exit(1);
+        m_exit(1);
     }
     const char* writefile = argv[1];
     const char* writestr = argv[2];
 
     write_file(writefile, writestr);
+
+    closelog();
 
     return 0;
 }
